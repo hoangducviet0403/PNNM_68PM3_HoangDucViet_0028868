2101b04c9cd8d7e028c0aea916b3faf51d811e26
hoangducviet0403 <helanakari0403@gmail.com>
2026-06-11 12:44:05 +0700

    Update sinhvien.php

--- app/controllers/sinhvien.php
+++ app/controllers/sinhvien.php
@@ -1,13 +1,25 @@
 <?php
-class sinhvien
-{
+require_once '../app/core/Controller.php';
+
+class sinhvien extends Controller {
+
     public function index()
     {
-        echo "Đây là trang sinh viên";
+        // 1. Gọi model 'sinhvienModel' dậy làm việc
+        $sinhvienModel = $this->model('sinhvienModel');
+
+        // 2. Chạy hàm lấy 22 sinh viên mồi dưới Database lên
+        $sinhviens = $sinhvienModel->getAllSinhVien();
+
+        // 3. Bắn cục dữ liệu sang file giao diện HTML/CSS để hiển thị
+        $this->view('sinhvien/index', [
+            'sinhviens' => $sinhviens
+        ]);
     }
 
     public function create()
     {
-        echo "Đây là trang tạo sinh viên";
+        require_once '../app/views/sinhvien/create.php';
     }
 }
+?>
