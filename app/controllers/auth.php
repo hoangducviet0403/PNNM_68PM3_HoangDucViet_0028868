5d7f538d610712ed1e2d933272002bae193d34b8
hoangducviet0403 <helanakari0403@gmail.com>
2026-06-11 13:33:45 +0700

    them logout

--- app/controllers/auth.php
+++ app/controllers/auth.php
@@ -1,34 +1,55 @@
 <?php
 class auth {
+
     protected $user = [
         "viethd" => "123456",
         "admin" => "123456"
     ];
 
-    public function login(){
+    public function login()
+    {
         session_start();
 
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
             $username = $_POST['username'];
             $password = $_POST['password'];
 
             if(isset($this->user[$username]) && $this->user[$username] == $password){
-                
+
                 $_SESSION['username'] = $username;
-                if(isset($_POST['remember']) && $_POST['remember']==true){
-                    setcookie('username', $username, time()+3600);
+
+                if(isset($_POST['remember'])){
+                    setcookie('username', $username, time() + 3600, '/');
                 }
+
                 header("Location: http://localhost/PNNM_68PM3_HoangDucViet_0028868/public/home/index");
                 exit();
-                
+
             } else {
+
                 header("Location: http://localhost/PNNM_68PM3_HoangDucViet_0028868/public/auth/login?error=1");
                 exit();
             }
-        } 
+        }
         else {
+
             require_once '../app/views/home/login.php';
         }
+    }
+
+    public function logout()
+    {
+        session_start();
+
+        $_SESSION = [];
+
+        session_destroy();
+
+        setcookie('username', '', time() - 3600, '/');
+
+        header("Location: http://localhost/PNNM_68PM3_HoangDucViet_0028868/public/auth/login");
+        exit();
     }
 }
 ?>
