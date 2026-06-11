e84636ef5e4a5ec63a7910331f9c932d6ee0b01e
hoangducviet0403 <helanakari0403@gmail.com>
2026-06-11 14:29:17 +0700

    update sinh vien

---
+++ app/views/sinhvien/edit.php
@@ -0,0 +1,35 @@
+<h1>Sửa sinh viên</h1>
+
+<form action="/PNNM_68PM3_HoangDucViet_0028868/public/sinhvien/update" method="POST">
+
+    <p>
+        <label>MSSV</label><br>
+        <input type="text" name="mssv" value="<?= $sinhvien['mssv'] ?>" readonly>
+    </p>
+
+    <br>
+
+    <p>
+        <label>Họ tên</label><br>
+        <input type="text" name="hoten" value="<?= $sinhvien['hoten'] ?>" required>
+    </p>
+
+    <br>
+
+    <p>
+        <label>Giới tính</label><br>
+
+        <input type="radio" name="gioitinh" value="Nam"
+            <?= $sinhvien['gioitinh'] == 'Nam' ? 'checked' : '' ?>>
+        Nam
+
+        <input type="radio" name="gioitinh" value="Nữ"
+            <?= $sinhvien['gioitinh'] == 'Nữ' ? 'checked' : '' ?>>
+        Nữ
+    </p>
+
+    <br>
+
+    <button type="submit">Cập nhật</button>
+
+</form>
