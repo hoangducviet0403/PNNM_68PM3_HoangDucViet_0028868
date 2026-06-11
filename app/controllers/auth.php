f6895a9ce5972f9f3eabd6a5ea75f04e2dd82034
hoangducviet0403 <helanakari0403@gmail.com>
2026-06-11 12:45:16 +0700

    add login feature

---
+++ app/controllers/auth.php
@@ -0,0 +1,34 @@
+<?php
+class auth {
+    protected $user = [
+        "viethd" => "123456",
+        "admin" => "123456"
+    ];
+
+    public function login(){
+        session_start();
+
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            if(isset($this->user[$username]) && $this->user[$username] == $password){
+                
+                $_SESSION['username'] = $username;
+                if(isset($_POST['remember']) && $_POST['remember']==true){
+                    setcookie('username', $username, time()+3600);
+                }
+                header("Location: http://localhost/PNNM_68PM3_HoangDucViet_0028868/public/home/index");
+                exit();
+                
+            } else {
+                header("Location: http://localhost/PNNM_68PM3_HoangDucViet_0028868/public/auth/login?error=1");
+                exit();
+            }
+        } 
+        else {
+            require_once '../app/views/home/login.php';
+        }
+    }
+}
+?>
