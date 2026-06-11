5cfeaaad316e87f0a270b0fcb928f3b5abbd703b
hoangducviet0403 <helanakari0403@gmail.com>
2026-06-11 12:43:47 +0700

    Update home.php

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,13 +1,19 @@
 <?php
 class home
 {
-    public function index()
-    {
-        echo "Đây là trang chủ";
-    }
+  public function index()
+  {
+    echo "Đây là trang chủ";
+  }
 
-    public function about()
-    {
-        echo "Đây là trang giới thiệu";
-    }
+  public function about()
+  {
+    echo "Đây là trang giới thiệu";
+  }
+  public function login()
+  {
+    require_once '../app/views/home/login.php';
+
+  }
 }
+?>
