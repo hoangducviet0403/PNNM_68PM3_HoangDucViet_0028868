8075aff8d89eafc8ca3ac25bfce4df20b45db3fe
hoangducviet0403 <helanakari0403@gmail.com>
2026-06-11 14:15:04 +0700

    Create sinhvien

--- app/models/sinhvienModel.php
+++ app/models/sinhvienModel.php
@@ -1,19 +1,33 @@
 <?php
-    require_once  '../app/core/Database.php';
+require_once '../app/core/Database.php';
 
-    class sinhvienModel {
-        private $conn;
+class sinhvienModel {
+    private $conn;
 
-        public function __construct() {
-           $this->conn = ConnectDB::connect();
-        }
-        public function getAllSinhVien() {
-            $query = "SELECT * FROM sinhvien";
-            $stmt = $this->conn->prepare($query);
-            $stmt->execute();
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        }
-        
+    public function __construct() {
+        $this->conn = ConnectDB::connect();
     }
 
+    public function getAllSinhVien() {
+        $query = "SELECT * FROM sinhvien";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function insertSinhVien($mssv, $hoten, $gioitinh)
+    {
+        $sql = "INSERT INTO sinhvien(mssv, hoten, gioitinh)
+                VALUES (:mssv, :hoten, :gioitinh)";
+
+        $stmt = $this->conn->prepare($sql);
+
+        return $stmt->execute([
+            ':mssv' => $mssv,
+            ':hoten' => $hoten,
+            ':gioitinh' => $gioitinh
+        ]);
+    }
+}
 ?>
