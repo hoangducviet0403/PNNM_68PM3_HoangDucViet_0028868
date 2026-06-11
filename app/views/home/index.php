2b663f3bbfa17d887cea53dae69bd397a82b19e8
hoangducviet0403 <helanakari0403@gmail.com>
2026-06-11 14:15:57 +0700

    Create sinhvien

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -8,3 +8,16 @@
 </p>
 
 <br>
+
+<a class="btn"
+   href="/PNNM_68PM3_HoangDucViet_0028868/public/sinhvien/index">
+    Xem danh sách sinh viên
+</a>
+
+<br>
+<br>
+
+<a class="btn"
+    href="/PNNM_68PM3_HoangDucViet_0028868/public/sinhvien/create">
+    Thêm sinh viên
+</a>
