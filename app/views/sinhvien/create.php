ecae9464c75f13c1411fa8c4ca9ec0a6bd3a8cbf
hoangducviet0403 <helanakari0403@gmail.com>
2026-06-19 20:13:28 +0700

    add lophoc

--- app/views/sinhvien/create.php
+++ app/views/sinhvien/create.php
@@ -3,33 +3,31 @@
 <form action="/PNNM_68PM3_HoangDucViet_0028868/public/sinhvien/store" method="POST">
 
     <p>
-        <label>MSSV</label><br>
-        <input type="text" name="mssv" required>
-    </p>
-
-    <br>
-
-    <p>
         <label>Họ tên</label><br>
         <input type="text" name="hoten" required>
     </p>
 
-    <br>
-
     <p>
         <label>Giới tính</label><br>
-
-        <input type="radio" name="gioitinh" value="Nam" checked>
-        Nam
-
-        <input type="radio" name="gioitinh" value="Nữ">
-        Nữ
+        <input type="radio" name="gioitinh" value="Nam" checked> Nam
+        <input type="radio" name="gioitinh" value="Nữ"> Nữ
     </p>
 
-    <br>
+    <p>
+        <label>MSSV</label><br>
+        <input type="text" name="mssv" required>
+    </p>
 
-    <button type="submit">
-        Thêm sinh viên
-    </button>
+    <p>
+        <label>Lớp</label><br>
+        <select name="lophoc_id" required>
+            <?php foreach ($lophocs as $lop): ?>
+                <option value="<?php echo $lop['id']; ?>">
+                    <?php echo $lop['malop'] . ' - ' . $lop['tenlop']; ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+    </p>
 
+    <button type="submit">Thêm sinh viên</button>
 </form>
