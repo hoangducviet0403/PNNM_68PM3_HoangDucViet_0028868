48e0198d29d26c82ac5f23bf630cf073465a1394
hoangducviet0403 <helanakari0403@gmail.com>
2026-06-19 20:35:40 +0700

    Update index.php

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,23 +1,17 @@
-<h1>Trang chủ</h1>
+<div class="card home-card">
+    <h1>Trang chủ</h1>
 
-<br>
+    <p>
+        Xin chào <strong><?= $_SESSION['username']; ?></strong>
+    </p>
 
-<p>
-    Xin chào
-    <strong><?= $_SESSION['username']; ?></strong>
-</p>
+    <a class="btn btn-primary"
+       href="/PNNM_68PM3_HoangDucViet_0028868/public/sinhvien/index">
+        Quản lý sinh viên
+    </a>
 
-<br>
-
-<a class="btn"
-   href="/PNNM_68PM3_HoangDucViet_0028868/public/sinhvien/index">
-    Xem danh sách sinh viên
-</a>
-
-<br>
-<br>
-
-<a class="btn"
-    href="/PNNM_68PM3_HoangDucViet_0028868/public/sinhvien/create">
-    Thêm sinh viên
-</a>
+    <a class="btn btn-success"
+       href="/PNNM_68PM3_HoangDucViet_0028868/public/lophoc/index">
+        Quản lý lớp học
+    </a>
+</div>
