da0c71d91591d9237ea31d84ea6171105d4f159a
hoangducviet0403 <helanakari0403@gmail.com>
2026-06-19 20:10:52 +0700

    Update sinhvien.php

--- app/controllers/sinhvien.php
+++ app/controllers/sinhvien.php
@@ -1,14 +1,14 @@
 <?php
 require_once '../app/core/Controller.php';
 
-class sinhvien extends Controller {
-
+class sinhvien extends Controller
+{
     public function index()
     {
         $sinhvienModel = $this->model('sinhvienModel');
+        $lophocModel = $this->model('lophocModel');
 
         $limit = 5;
-
         $page = $_GET['page'] ?? 1;
         $page = (int)$page;
 
@@ -16,61 +16,47 @@ class sinhvien extends Controller {
             $page = 1;
         }
 
+        $keyword = $_GET['keyword'] ?? '';
+        $lophoc_id = $_GET['lophoc_id'] ?? '';
+
         $offset = ($page - 1) * $limit;
 
-        $totalSinhVien = $sinhvienModel->countSinhVien();
+        $totalSinhVien = $sinhvienModel->countSinhVien($keyword, $lophoc_id);
         $totalPage = ceil($totalSinhVien / $limit);
 
-        $sinhviens = $sinhvienModel->getSinhVienPaging($limit, $offset);
+        $sinhviens = $sinhvienModel->getSinhVienPaging($limit, $offset, $keyword, $lophoc_id);
+        $lophocs = $lophocModel->getAllLopHoc();
 
         $this->view('sinhvien/index', [
             'sinhviens' => $sinhviens,
+            'lophocs' => $lophocs,
             'page' => $page,
-            'totalPage' => $totalPage
+            'totalPage' => $totalPage,
+            'keyword' => $keyword,
+            'lophoc_id' => $lophoc_id
         ]);
     }
 
     public function create()
     {
-        $this->view('sinhvien/create');
+        $lophocModel = $this->model('lophocModel');
+        $lophocs = $lophocModel->getAllLopHoc();
+
+        $this->view('sinhvien/create', [
+            'lophocs' => $lophocs
+        ]);
     }
 
     public function store()
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $hoten = $_POST['hoten'] ?? '';
-            $gioitinh = $_POST['gioitinh'] ?? '';
-            $mssv = $_POST['mssv'] ?? '';
-
-            $sinhvienModel = $this->model('sinhvienModel');
-            $sinhvienModel->insertSinhVien($hoten, $gioitinh, $mssv);
-
-            header('Location: /PNNM_68PM3_HoangDucViet_0028868/public/sinhvien/index');
-            exit();
-        }
-    }
-
-    public function edit($mssv)
-    {
-        $sinhvienModel = $this->model('sinhvienModel');
-
-        $sinhvien = $sinhvienModel->getSinhVienByMssv($mssv);
-
-        $this->view('sinhvien/edit', [
-            'sinhvien' => $sinhvien
-        ]);
-    }
-
-    public function update()
-    {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
             $sinhvienModel = $this->model('sinhvienModel');
 
-            $sinhvienModel->updateSinhVien(
+            $sinhvienModel->insertSinhVien(
                 $_POST['mssv'],
                 $_POST['hoten'],
-                $_POST['gioitinh']
+                $_POST['gioitinh'],
+                $_POST['lophoc_id']
             );
 
             header('Location: /PNNM_68PM3_HoangDucViet_0028868/public/sinhvien/index');
@@ -78,11 +64,42 @@ class sinhvien extends Controller {
         }
     }
 
-    public function delete($mssv)
+    public function edit($id)
     {
         $sinhvienModel = $this->model('sinhvienModel');
+        $lophocModel = $this->model('lophocModel');
 
-        $sinhvienModel->deleteSinhVien($mssv);
+        $sinhvien = $sinhvienModel->getSinhVienById($id);
+        $lophocs = $lophocModel->getAllLopHoc();
+
+        $this->view('sinhvien/edit', [
+            'sinhvien' => $sinhvien,
+            'lophocs' => $lophocs
+        ]);
+    }
+
+    public function update()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $sinhvienModel = $this->model('sinhvienModel');
+
+            $sinhvienModel->updateSinhVien(
+                $_POST['id'],
+                $_POST['mssv'],
+                $_POST['hoten'],
+                $_POST['gioitinh'],
+                $_POST['lophoc_id']
+            );
+
+            header('Location: /PNNM_68PM3_HoangDucViet_0028868/public/sinhvien/index');
+            exit();
+        }
+    }
+
+    public function delete($id)
+    {
+        $sinhvienModel = $this->model('sinhvienModel');
+        $sinhvienModel->deleteSinhVien($id);
 
         header('Location: /PNNM_68PM3_HoangDucViet_0028868/public/sinhvien/index');
         exit();
