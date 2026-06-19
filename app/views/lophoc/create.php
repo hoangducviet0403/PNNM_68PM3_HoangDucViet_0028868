f74e8eb8e0dbbc27641e100f023427dc3a274284
hoangducviet0403 <helanakari0403@gmail.com>
2026-06-19 20:40:12 +0700

    style

--- app/views/lophoc/create.php
+++ app/views/lophoc/create.php
@@ -1,24 +1,27 @@
-<h1>Thêm lớp học</h1>
 
-<form action="/PNNM_68PM3_HoangDucViet_0028868/public/lophoc/store"
+<h1 class="page-title">Thêm lớp học</h1>
+
+<div class="card form-box">
+    <form action="/PNNM_68PM3_HoangDucViet_0028868/public/lophoc/store"
       method="POST">
 
-    <p>
-        <label>Mã lớp</label><br>
-        <input type="text" name="malop" required>
-    </p>
+        <p>
+            <label>Mã lớp</label><br>
+            <input class="form-control" type="text" name="malop" required>
+        </p>
 
-    <br>
+        <br>
 
-    <p>
-        <label>Tên lớp</label><br>
-        <input type="text" name="tenlop" required>
-    </p>
+        <p>
+            <label>Tên lớp</label><br>
+            <input class="form-control" type="text" name="tenlop" required>
+        </p>
 
-    <br>
+        <br>
 
-    <button type="submit">
-        Thêm lớp học
-    </button>
+        <button class="btn btn-success" type="submit">
+            Thêm lớp học
+        </button>
 
-</form>
+    </form>
+</div>
