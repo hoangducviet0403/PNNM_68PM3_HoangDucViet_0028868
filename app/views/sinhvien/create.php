54767737fa7c80c342bd2deaaa2b7352841b888d
hoangducviet0403 <helanakari0403@gmail.com>
2026-06-19 20:41:03 +0700

    style

--- app/views/sinhvien/create.php
+++ app/views/sinhvien/create.php
@@ -1,33 +1,36 @@
-<h1>Thêm sinh viên</h1>
 
-<form action="/PNNM_68PM3_HoangDucViet_0028868/public/sinhvien/store" method="POST">
+<h1 class="page-title">Thêm sinh viên</h1>
 
-    <p>
-        <label>Họ tên</label><br>
-        <input type="text" name="hoten" required>
-    </p>
+<div class="card form-box">
+    <form action="/PNNM_68PM3_HoangDucViet_0028868/public/sinhvien/store" method="POST">
 
-    <p>
-        <label>Giới tính</label><br>
-        <input type="radio" name="gioitinh" value="Nam" checked> Nam
-        <input type="radio" name="gioitinh" value="Nữ"> Nữ
-    </p>
+        <p>
+            <label>Họ tên</label><br>
+            <input class="form-control" type="text" name="hoten" required>
+        </p>
 
-    <p>
-        <label>MSSV</label><br>
-        <input type="text" name="mssv" required>
-    </p>
+        <p>
+            <label>Giới tính</label><br>
+            <input type="radio" name="gioitinh" value="Nam" checked> Nam
+            <input type="radio" name="gioitinh" value="Nữ"> Nữ
+        </p>
 
-    <p>
-        <label>Lớp</label><br>
-        <select name="lophoc_id" required>
-            <?php foreach ($lophocs as $lop): ?>
-                <option value="<?php echo $lop['id']; ?>">
-                    <?php echo $lop['malop'] . ' - ' . $lop['tenlop']; ?>
-                </option>
-            <?php endforeach; ?>
-        </select>
-    </p>
+        <p>
+            <label>MSSV</label><br>
+            <input class="form-control" type="text" name="mssv" required>
+        </p>
 
-    <button type="submit">Thêm sinh viên</button>
-</form>
+        <p>
+            <label>Lớp</label><br>
+            <select class="form-control" name="lophoc_id" required>
+                <?php foreach ($lophocs as $lop): ?>
+                    <option value="<?php echo $lop['id']; ?>">
+                        <?php echo $lop['malop'] . ' - ' . $lop['tenlop']; ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+        </p>
+
+        <button class="btn btn-success" type="submit">Thêm sinh viên</button>
+    </form>
+</div>
